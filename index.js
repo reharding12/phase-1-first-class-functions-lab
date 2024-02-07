11395e800f2d1730494de9f7998f02b12a790d03
Roxanne <reharding12@gmail.com>
2024-02-07 14:15:20 -0700

    passed 13 tests

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const drivers = ('Antonia', 'Nuru', 'Amari', 'Mo');
+
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+      return fare * multiplier;
+    };
+  }
+
+  function fareDoubler(fare) {
+    return fare * 2;
+  }
+
+  function fareTripler(fare) {
+    return fare * 3;
+  }
+
+  function selectDifferentDrivers(drivers, driverSelector) {
+    return driverSelector(drivers);
+  }
